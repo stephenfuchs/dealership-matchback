706a8a4d147f0e96324ec2b6ff2126e0f14df26a
Stephen Fuchs <stephen@stephenfuchs.net>
2022-03-23 10:30:20 -0500

    remove clean-webpack-plugin in favor of native webpack clean function

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,12 +1,9 @@
 const Path = require("path");
-// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {
-    app: Path.resolve(__dirname, "../src/scripts/index.js"),
-  },
+  entry: Path.resolve(__dirname, "../src/scripts/index.js"),
   output: {
     path: Path.join(__dirname, "../dist"),
     filename: "js/[name].js",
@@ -19,7 +16,6 @@ module.exports = {
     },
   },
   plugins: [
-    // new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
       patterns: [
         { from: Path.resolve(__dirname, "../src/images"), to: "images" },
