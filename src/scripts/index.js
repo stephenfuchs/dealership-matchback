527cb31b2c870892f13c709044e13ee8c944fa73
Stephen Fuchs <stephen@stephenfuchs.net>
2022-03-30 08:45:52 -0500

    add form JavaScript functions to fill data into html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,4 +7,31 @@ const elem = document.getElementById("dateRange");
 new DateRangePicker(elem, {
   buttonClass: "btn",
   allowOneSidedRange: true,
+  format: {
+    toValue(date) {
+      return DateRangePicker.formatDate(date, "MM dd, y");
+    },
+  },
 });
+
+let dealership = document.querySelector(".header__dealership");
+let startDate = document.querySelector(".header__dates--start");
+let endDate = document.querySelector(".header__dates--end");
+
+const addClient = (event) => {
+  event.preventDefault();
+
+  let clientData = {
+    client: document.getElementById("clientName").value,
+    start: document.getElementById("startDate").value,
+    end: document.getElementById("endDate").value,
+  };
+  console.log(clientData.client);
+
+  dealership.innerHTML = clientData.client;
+  startDate.innerHTML = clientData.start;
+  endDate.innerHTML = clientData.end;
+};
+
+let form = document.querySelector("form");
+form.addEventListener("submit", addClient);
