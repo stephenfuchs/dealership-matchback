49eeabcaedb88db64432e682089b8b173368fee1
Stephen Fuchs <stephen@stephenfuchs.net>
2022-04-08 12:30:03 -0500

    add csv parse and table creation from data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,40 @@
 import "../styles/index.scss";
+import Papa from "papaparse";
+import html2pdf from "html2pdf.js";
 
-import * as bootstrap from "bootstrap";
-
+const startup = document.getElementById("startup");
+const app = document.getElementById("app");
 const form = document.querySelector("form");
 const dealership = document.querySelector(".header__dealership");
 const startDate = document.querySelector(".header__dates--start");
 const endDate = document.querySelector(".header__dates--end");
 const btnNew = document.getElementById("btnNew");
+const btnModify = document.getElementById("btnModify");
+const btnPDF = document.getElementById("btnPDF");
+const pdfOutput = document.getElementById("pdfOutput");
+const tableNew = document.querySelector(".buyers-new__table");
+const tableUsed = document.querySelector(".buyers-used__table");
 
-var myModal = new bootstrap.Modal(document.getElementById("clientForm"));
-myModal.show();
-// Form Functionality
+// TOGGLE DISPLAY
+function toggleApp() {
+  app.classList.toggle("d-none");
+  startup.classList.toggle("d-none");
+}
+
+// RESET APP
+function reset() {
+  if (
+    confirm(
+      `Are you sure you want to create a new report? The current data will be erased`
+    )
+  ) {
+    location.reload();
+  }
+}
+
+btnNew.addEventListener("click", reset);
+
+// FORM FUNCTIONALITY
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
@@ -32,15 +56,71 @@ form.addEventListener("submit", function (event) {
   dealership.innerHTML = clientData.client;
   startDate.innerHTML = formatDate(clientData.start);
   endDate.innerHTML = formatDate(clientData.end);
+
+  parseCSV();
+  toggleApp();
 });
 
-// Reset form
-btnNew.addEventListener("click", function () {
-  if (
-    confirm(
-      `Are you sure you want to create a new report? The current data will be erased`
-    )
-  ) {
-    location.reload();
-  }
-});
+// MODIFY DATA
+btnModify.addEventListener("click", toggleApp);
+
+// CREATE PDF
+function createPDF() {
+  pdfOutput.classList.add("pdf");
+  const opt = {
+    margin: 0,
+    filename: "myfile.pdf",
+    image: { type: "jpeg", quality: 1 },
+    html2canvas: {
+      allowTaint: true,
+      scale: 2,
+      useCORS: true,
+    },
+    pagebreak: {
+      before: [".buyers-new", ".buyers-used"],
+    },
+    jsPDF: {
+      unit: "in",
+      format: "letter",
+    },
+  };
+  html2pdf()
+    .set(opt)
+    .from(pdfOutput)
+    .save()
+    .then(function () {
+      pdfOutput.classList.remove("pdf");
+    });
+}
+
+btnPDF.addEventListener("click", createPDF);
+
+// PARSE CSV DATA
+function parseCSV() {
+  Papa.parse(document.getElementById("csvFile").files[0], {
+    download: true,
+    header: true,
+    complete: function (results) {
+      let isNew = results.data.filter((purchase) => purchase.condition == "N");
+      let isUsed = results.data.filter((purchase) => purchase.condition == "U");
+
+      createTableData(isNew, tableNew);
+      createTableData(isUsed, tableUsed);
+    },
+  });
+}
+
+// CREATE TABLE FROM DATA
+function createTableData(condition, location) {
+  condition.forEach((cust) => {
+    let row = document.createElement("tr");
+
+    Object.values(cust).forEach((text) => {
+      let cell = document.createElement("td");
+      let textNode = document.createTextNode(text);
+      cell.appendChild(textNode);
+      row.appendChild(cell);
+    });
+    location.appendChild(row);
+  });
+}
